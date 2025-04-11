d9c190cfceb489c82df869887a87c3e9ca51c41f
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-04-11 12:58:37 +0100

    Mettre en place React Router

--- my-movie-app/src/App.jsx
+++ my-movie-app/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import MovieDetails from './components/MovieDetails';
+import FavorisMovie from './components/FavorisMovie';
 
 function App() {
+  const [type, setType] = useState('movie');         
+  const [searchTerm, setSearchTerm] = useState('');  
+
+  useEffect(() => {
+    const handleSearch = (event) => {
+      setSearchTerm(event.detail.term);
+    };
+
+    window.addEventListener('movie-search', handleSearch);
+
+    return () => {
+      window.removeEventListener('movie-search', handleSearch);
+    };
+  }, []);
+
   return (
-    <div>
-      
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-black dark:text-white">
+      <Header onFilterChange={setType} />
+
+      <Routes>
+        <Route path="/" element={<Home type={type} searchTerm={searchTerm} />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/favoris" element={<FavorisMovie />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-movie-app/src/components/FavorisMovie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FavorisMovie() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FavorisMovie
